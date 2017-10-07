e987ae0ab3679067526164ef67be929b4401b48e
parinz1234 <parinya.ons@gmail.com>
2017-10-07 20:55:37 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Layout from '../components/MyLayout'
+
+const PostLink = (props) => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+)
+
+const Index = () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js"/>
+      <PostLink title="Learn Next.js is awesome"/>
+      <PostLink title="Deploy apps with Zeit"/>
+    </ul>
+  </Layout>
+)
+
+export default Index
